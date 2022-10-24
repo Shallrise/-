674eaa23282db4ec1d365bb6476119d60faf78d5
Shallrise <1751953786@qq.com>
2022-10-24 19:46:26 +0800

    修改类型

--- src/type/classType.ts
+++ src/type/classType.ts
@@ -1,49 +1,55 @@
-interface recodsInt{
-    hot:number,
-    hour:number,
-    id:number,
-    image:string,
-    kind:number,
-    teacher:string,
-    title:string
+interface recodsInt {
+  hot: number;
+  hour: number;
+  id: number;
+  image: string;
+  kind: number;
+  teacher: string;
+  title: string;
 }
 
-interface pageDataInt{
-    current:number,
-    size:number,
-    total:number
+interface pageDataInt {
+  current: number;
+  size: number;
+  total: number;
 }
 
-export class ClassData{
-    pageData:pageDataInt={
-        current:0,
-        size:10,
-        total:2
-    }
-    records:recodsInt[]=[]
+export class ClassData {
+  pageData: pageDataInt = {
+    current: 0,
+    size: 10,
+    total: 2,
+  };
+  records: recodsInt[] = [];
 }
 
-export interface addformInt{
-    title:string,
-    teacher:string,
-    hour:string,
-    image:string,
-    introduce:string
+export interface addformInt {
+  title: string;
+  teacher: string;
+  hour: string;
+  image: string;
+  introduce: string;
 }
 
-export class addformData{
-    addForm:addformInt[]=[]
+export class addformData {
+  addForm: addformInt = {
+    title: "",
+    teacher:"",
+    hour:"",
+    image:'',
+    introduce:''
+  };
 }
 
-export interface eddformInt{
-    id: number,
-    image: string,
-    introduce:string,
-    teacher: string,
-    hour:number,
-    title:string
+export interface eddformInt {
+  id: number;
+  image: string;
+  introduce: string;
+  teacher: string;
+  hour: number;
+  title: string;
 }
 
-export class eddformData{
-    editForm:eddformInt[]=[]
+export class eddformData {
+  editForm: eddformInt[] = [];
 }
