179db9cafbdf938c60faf9007139e78ae50d5579
Shallrise <1751953786@qq.com>
2022-10-27 15:16:34 +0800

    修改传值number

--- src/views/Chapter.vue
+++ src/views/Chapter.vue
@@ -73,7 +73,7 @@ import { getChapter, addChapter, eddChapter } from "../api/chapter";
 import { ChapterData, addformData, eddformData,eddformInt } from "../type/chapterType";
 
 const router = useRoute();
-const courseId = ref<number>(router.query.courseId);
+const courseId:number = ref<number>(router.query.courseId);
 const chapterDataList = reactive(new ChapterData());
 const addFormList = reactive(new addformData());
 const eddFormList = reactive(new eddformData());
