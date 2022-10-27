24711cd41209cd8173f642cda1dae77d67952c1f
Shallrise <1751953786@qq.com>
2022-10-27 16:09:29 +0800

    reactive修改

--- src/views/Chapter.vue
+++ src/views/Chapter.vue
@@ -73,7 +73,7 @@ import { getChapter, addChapter, eddChapter } from "../api/chapter";
 import { ChapterData, addformData, eddformData,eddformInt } from "../type/chapterType";
 
 const router = useRoute();
-const courseId:number =ref(router.query.id);
+const courseId:number =reactive(router.query.id);
 const chapterDataList = reactive(new ChapterData());
 const addFormList = reactive(new addformData());
 const eddFormList = reactive(new eddformData());
@@ -89,7 +89,7 @@ const state = reactive<{
 const { addDialogVisible, eddDialogVisible } = toRefs(state);
 
 const getChapterList = () => {
-  getChapter(courseId.value)
+  getChapter(courseId)
     .then((res: any) => {
       chapterDataList.records = res.data.data.records;
     })
